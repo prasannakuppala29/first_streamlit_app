dc3c139f64488f460b481db930fa9035da8af27d
prasannakuppala29 <125112290+prasannakuppala29@users.noreply.github.com>
2023-02-11 11:17:23 +0530

    Update streamlit_app.py
    added some benefits of database

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
 streamlit.title(" I'm loving Snowflakes DB")
+Benefits of the Database 
+1) Perfect Saas product 
+2) High Performance 
+3) Mainenance Free 
+4) Pay for what you use.!
