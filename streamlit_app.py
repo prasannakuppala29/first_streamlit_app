0845997984218b5b23ffb871a91555b52ea007b4
prasannakuppala29 <125112290+prasannakuppala29@users.noreply.github.com>
2023-02-11 11:20:47 +0530

    Update streamlit_app.py
    fixed few other errors

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,12 @@
 import streamlit
 streamlit.title(" I'm loving Snowflakes DB")
-Benefits of the Database 
-Perfect Saas product 
-High Performance 
-Mainenance Free 
-Pay for what you use.!
+streamlit.header(" Benefits of the Database") 
+streamlit.text(" Perfect Saas product") 
+streamlit.text(" High Performance") 
+streamlit.text(" Miantenance Free ") 
+streamlit.text(" Pay for what you use.!") 
+
+ 
+ 
+
+
