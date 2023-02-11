f2947b5fae4e7732817b15f07e30854374a2a1d0
prasannakuppala29 <125112290+prasannakuppala29@users.noreply.github.com>
2023-02-11 11:18:10 +0530

    Update streamlit_app.py
    rectified the errors

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 streamlit.title(" I'm loving Snowflakes DB")
 Benefits of the Database 
-1) Perfect Saas product 
-2) High Performance 
-3) Mainenance Free 
-4) Pay for what you use.!
+Perfect Saas product 
+High Performance 
+Mainenance Free 
+Pay for what you use.!
