7d8fd709f9fb89798924d97247dd6b05358c5ada
prasannakuppala29 <125112290+prasannakuppala29@users.noreply.github.com>
2023-02-11 11:23:54 +0530

    Update streamlit_app.py
    added breakfast menu  header  and 3 items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,11 @@
 import streamlit
-streamlit.title(" I'm loving Snowflakes DB")
-streamlit.header(" Benefits of the Database") 
-streamlit.text(" Perfect Saas product") 
-streamlit.text(" High Performance") 
-streamlit.text(" Miantenance Free ") 
-streamlit.text(" Pay for what you use.!") 
+
+streamlit.title(" My Parents New Healthy Dinner")
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+
 
  
  
