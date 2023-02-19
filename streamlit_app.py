b90be48d38858364bf679fcf740ebe1e2f89755e
prasannakuppala29 <125112290+prasannakuppala29@users.noreply.github.com>
2023-02-19 18:32:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,3 +40,5 @@ streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
 fruit_choice = streamlit.text_input('What fruit would you like information about?','jackfruit')
 streamlit.write('Thanks for adding', fruit_choice)
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+
